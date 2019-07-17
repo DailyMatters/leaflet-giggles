6b70df0e0f6949a0b37e1c6cb38679d530f4447f
Claudio Ribeiro <cribeiro@vilagale.com>
2019-07-17 16:46:45 +0100

    Minor changes

--- parse.php
+++ parse.php
@@ -17,8 +17,7 @@ foreach (glob("hotels/*.php") as $filename)
 	$result_array = json_encode($result_array);
 
 	//Save tags somewhere else
-	$filename_without_extension = pathinfo($filename, PATHINFO_FILENAME);
-	file_put_contents('hotels/' . $filename_without_extension . '.json', $result_array);
+	file_put_contents('hotels/' . pathinfo($filename, PATHINFO_FILENAME) . '.json', $result_array);
 }
 
 ?>
