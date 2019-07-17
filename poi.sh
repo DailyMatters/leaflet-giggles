c641e08ddb4d5d60021fc55932efb7cde67820f0
Claudio Ribeiro <cribeiro@vilagale.com>
2019-07-17 15:33:10 +0100

    Add another hotel

--- poi.sh
+++ poi.sh
@@ -3,13 +3,13 @@
 declare -A HOTELS
 HOTELS[Ericeira]='http://overpass-api.de/api/interpreter?data=node["highway"!~"."]["created_by"!="JOSM"](38.956,-9.421,38.969,-9.402);out;'
 HOTELS[Tavira]='http://overpass-api.de/api/interpreter?data=node["highway"!~"."]["created_by"!="JOSM"](37.111051,-7.661719,37.128913,-7.618461);out;'
+HOTELS[ClubeCampo]='http://overpass-api.de/api/interpreter?data=node["highway"!~"."]["created_by"!="JOSM"](37.847341,-8.079071,37.913800,-7.921314);out;'
 # All the other hotels come here
 
 for hotel in "${!HOTELS[@]}"
 do
   echo "Key    : $hotel"
   echo "Value  : ${HOTELS[$hotel]}"
-  echo "String : $hotel.php"
 
   wget --output-document=$hotel.php ${HOTELS[$hotel]}
 
